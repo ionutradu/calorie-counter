9887d5663e794bb4de51f761da170f3ed5a26fa5
ionut.radu <ionut.radu2712@gmail.com>
2016-12-30 22:07:38 +0200

    Initial commit

---
+++ src/main/java/ro/mpsit/CalorieCounterApplication.java
@@ -0,0 +1,12 @@
+package ro.mpsit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalorieCounterApplication.class, args);
+	}
+}
